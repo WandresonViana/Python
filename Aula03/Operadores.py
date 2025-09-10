173e0aa28f598f68c90fd613447010a0224b5d99
Wandreson Viana <wandresonviana01@gmail.com>
2025-09-09 21:31:26 -0300

    Operadores aritméticos
    Soma, subtração, divisão e multiplicação

---
+++ Aula03/Operadores.py
@@ -0,0 +1,10 @@
+nun1 = int(input('Digite o primeiro '))
+nun2 = int(input('Digite o segundo valor '))
+soma = nun1 + nun2
+sub = nun1 - nun2
+div = nun1 / nun2
+mult = nun1 * nun2
+media = (nun1 + nun2)/ 2
+print('A soma é {}, a subtração é {}, a divição é {} e a multiplicação é {}'.format(soma, sub, div, mult))
+print('A media de {} e {} é igual a {}'.format(nun1, nun2, media))
+print('O antecessor de {} é {} e o sucessor é {}'.format(nun1, nun1-1, nun1 + 1))
